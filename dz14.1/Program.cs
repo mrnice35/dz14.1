7a10c3d242c1d70300d1d9b72bd7aafc1afad142
gavrilenkovi@bk.ru <gavrilenkovi@bk.ru>
2021-11-11 23:56:52 +0300

    Добавьте файлы проекта.

---
+++ dz14.1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz14._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dogs dogs = new Dogs();
+            dogs.Name = "собака";
+            dogs.ShowInfo();
+            
+            Cats cats = new Cats();
+            cats.Name = "кошка";
+            cats.ShowInfo();
+            
+            Console.ReadKey();
+        }
+    }
+    abstract class Animal
+    {
+        public abstract string Name { get; set; }
+        public abstract void Say();
+        public void ShowInfo()
+        {
+            Console.WriteLine("класс животного:{0} ",Name);
+            Console.Write("говорит "); Say();
+        }
+    }
+    class Dogs:Animal
+    {
+        string name;
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public override void Say()
+        {
+            Console.WriteLine("Гав");
+        }
+    }
+    class Cats : Animal
+    {
+        string name;
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public override void Say()
+        {
+            Console.WriteLine("Мяу");
+        }
+    }
+}
